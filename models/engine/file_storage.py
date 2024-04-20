e1ee74df9b86689bcb1025bb2eeebf443cc1c226
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 11:27:25 +0200

    del

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,10 +65,9 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
